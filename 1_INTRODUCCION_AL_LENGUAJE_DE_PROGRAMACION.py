e8f42cf95552561f6db2497b847ccc0e2b672ef0
Emartinez08 <A01552626@itesm.mx>
2024-12-18 23:38:50 -0600

    ejercicio 1

---
+++ 1_INTRODUCCION_AL_LENGUAJE_DE_PROGRAMACION.py
@@ -0,0 +1,69 @@
+#INTRODUCCION AL LENGUAJE DE PROGRAMACION
+#OPERACIONES ARITMETICAS
+#OPERACIONES RELACIONALES
+#OPERACIONES LOGICAS
+
+#Enrique Martinez Luna
+
+import random
+
+X = random.randint(0,25)
+print ("X",X)
+Y = random.randint(0,25)
+print ("Y",Y)
+Z = random.randint(0,25)
+print ("Z",Z)
+A = random.randint(0,25)
+print ("A",A)
+B = random.randint(0,25)
+print ("B",B)
+C = random.randint(0,25)
+print ("C",C)
+
+SUMA = X + Y 
+
+print("SUMA",SUMA)
+
+RESTA = A - B 
+
+print("RESTA",RESTA)
+
+MULTIPLICACION = Z * C
+
+print ("MULTIPLICACION",MULTIPLICACION)
+
+DIVISION = B / A
+
+print ("DIVISION",DIVISION)
+
+POTENCIA = A ** 2
+
+print("POTENCIA",POTENCIA)
+
+DIVISION_ENTERA = A // Z
+
+print("DIVISION_ENTERA",DIVISION_ENTERA)
+
+IGUAL = A == B
+
+print ("IGUAL",IGUAL)
+
+MAYOR =  B > C
+
+print("MAYOR",MAYOR)
+
+MENOR = X < Y
+
+print ("MENOR",MENOR)
+
+MAYOR_IGUAL = Z >= A
+
+print ("MAYOR_IGUAL",MAYOR_IGUAL)
+
+MENOR_IGUAL = B <= C
+
+print ("MENOR_IGUAL",MENOR_IGUAL)
+
+DIFERENTE = Y != Z
+
+print ("DIFERENTE",DIFERENTE)
